8927f6206764a6e85b0a8b06b49df91b3472ed68
Luciel Filho <93838442+Lucifilho@users.noreply.github.com>
2023-04-28 00:36:24 -0300

    adiciona arquivos

---
+++ drupal/web/sites/default/files/php/twig/644b2b31020d3_page.html.twig_panVD8qZpOyBHlU2ClDj482a4/KsEj4fBvQoutTrEpeJKbiFbWx8Rh11c6ai2ez0c7xA0.php
@@ -0,0 +1,106 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/nestle/templates/layout/page.html.twig */
+class __TwigTemplate_1d058c083d91b2572459e5b4254a6423 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 46
+        echo "<div class=\"layout-container\">
+
+  <header role=\"banner\">
+  </header>
+
+  <main role=\"main\">
+
+    <div class=\"layout-content\">
+      ";
+        // line 54
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["page"] ?? null), "content", [], "any", false, false, true, 54), 54, $this->source), "html", null, true);
+        echo "
+    </div>
+
+  </main>
+
+
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/nestle/templates/layout/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  49 => 54,  39 => 46,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/nestle/templates/layout/page.html.twig", "E:\\htdocs\\drupal\\web\\themes\\nestle\\templates\\layout\\page.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 54);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
